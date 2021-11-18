f2ef49e959c04b7d7028d86e6b58120d3f826fed
dmitriy <Evgrashin.Dmitriy@yandex.ru>
2021-11-19 02:29:00 +0300

    new project

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const   hamburger = document.querySelector('.hamburger'),
         menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.menu__close');
+        closeElem = document.querySelector('.menu__close'),
         closeOverlay = document.querySelector('.menu__overlay');
 
         
@@ -21,3 +21,25 @@ const counters = document.querySelectorAll('.work__percent_figure'),
 counters.forEach( (item , i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+const body = document.documentElement,
+      sidepanel = document.querySelectorAll('.sidepanel path'),
+      divider = document.querySelector('.sidepanel__divider');
+      sidepanelText = document.querySelector('.sidepanel__text');
+
+    window.addEventListener('scroll', () => {
+        if (body.scrollTop >= 422) {
+            sidepanel.forEach(item => {
+                item.style.fill = 'black';
+            });
+            divider.style.background = 'black';
+            sidepanelText.style.color = 'black';
+        } else {
+            sidepanel.forEach(item => {
+                item.style.fill = 'white';
+            });
+            divider.style.background = 'white';
+            sidepanelText.style.color = 'white';
+        }
+        console.dir(event);
+    });
